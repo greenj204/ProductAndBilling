a50f92d20eba9ecfed27fbaa169c172a9a3f4938
greenj204 <james.fgreen@live.com>
2017-01-04 19:00:29 +0000

    Update DBconnect.scala

--- DBconnect.scala
+++ DBconnect.scala
@@ -4,31 +4,41 @@ import java.sql.DriverManager
 import java.sql.Connection
 
 /**
- * 
+ * Setting Database connection constants
  */
+class DBConstants{
+    def Driver: String = {"com.mysql.jdbc.Driver"}
+    def URL: String = {"jdbc:mysql://localhost:8889/ProductAndBilling"}
+    def USERNAME: String = {"root"}
+    def PASSWORD: String = {"root"}
+}
+        
 class ScalaJdbcConnectSelect{
 
     // connect to the database named "mysql" on the localhost
-    val driver = "com.mysql.jdbc.Driver"
-    val url = "jdbc:mysql://localhost:8889/SimpleBookKeeping"
-    val username = "root"
-    val password = "root"
+    // Declare constants
+    val Constants = new DBConstants()
+    val driver = Constants.DRIVER
+    val url = Constants.URL
+    val username = Constants.USERNAME
+    val password = Constants.PASSWORD
 
     // there's probably a better way to do this
     var connection:Connection = null
   
- def setConnection: Boolean={
-    var PassFail:Boolean = true
+ def setConnection: Unit={
+  
     try {
       // make the connection
       Class.forName(driver)
       connection = DriverManager.getConnection(url, username, password)
     } catch {
-      PassFail = false
       case e=> e.printStackTrace
     }
   }   //setConnection end
   
+  def giveConnection: Connection = { connection }
+    
   def ExecQuery(connection: Connection, Query: String)  = {
          // create the statement, and run the select query
       val statement = connection.createStatement()
@@ -37,9 +47,9 @@ class ScalaJdbcConnectSelect{
                                              "WHERE BookCode = 'GENEXP' ")
                                              */
       while ( resultSet.next() ) {
-        val host = resultSet.getString("BookCodeDebitAcc")
-        val user = resultSet.getString("BookCodeCreditAcc")
-        println("host, user = " + host + ", " + user)
+        val host = resultSet.getString("")
+        val user = resultSet.getString("")
+        println()
       }  
  }
  def CloseConnection Unit={  connection.close() }
